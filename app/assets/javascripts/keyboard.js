b6c59e9561053628736f958e385fe29223fa679a
Joaquin Alonso <jalonso1@miuandes.cl>
2019-05-09 17:05:58 -0400

    falta backspace

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,4 +1,20 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
 $(() => {
 	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+	$('.key').click(function()
+		{
+			console.log($(this).text());
+
+			var letter = ""
+			var textfield = $(this).text()
+
+			if (textfield=="Bksp"){
+				letter = $('#onscreenkeys').text().slice(0,-1) ;
+			}
+			else {
+				letter =  $('#onscreenkeys').text() + textfield;
+			}
+	    $('#onscreenkeys').text(letter) ;
+
+})
 });
